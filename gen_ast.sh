e082dd13798a427e56946e590903d62e5f580d1f
EugeneDar <evgenedar@mail.ru>
2022-11-21 21:36:22 +0300

    all files parsing

--- gen_ast.sh
+++ gen_ast.sh
@@ -1,4 +1,15 @@
 export TERM=xterm-mono
 
-#clang++ -Xclang -ast-dump -fsyntax-only data/simple.cpp > input/simple.cpp
-clang++ -Xclang -ast-dump -fsyntax-only data/simple.cpp
+#clang++ -Xclang -ast-dump -fsyntax-only data/simple_heap_allocation.cpp > input/simple_heap_allocation
+#clang++ -Xclang -ast-dump -fsyntax-only data/simple.cpp
+
+for file in `find data -type f -name "*"`
+do
+  prefix="data"
+  path="$file"
+  path="${path##data}"
+  input_file="data${path}"
+  output_file="input${path}"
+  echo $input_file $output_file
+  clang++ -Xclang -ast-dump -fsyntax-only ${input_file} > ${output_file}
+done
