ee5599b7034aebf491177ad13fdc81f84521df67
Eugene Dar <evgenedar@mail.ru>
2022-12-17 20:59:49 +0300

    main results printed

--- src/main.py
+++ src/main.py
@@ -1,25 +1,22 @@
+import numpy as np
+import os
+
 from parser import parse
 from counter import count_allocations
 from cpp_finder import get_cpps_list
 from ast_builder import build_ast
 from data_collector import collect
-
-
+from preprocessor import preprocess
 
 # collect repos with cpp files
 collect()
 
-
-
 # find all cpps
 cpps = get_cpps_list()
 
-
-
 # preprocess (wrap includes)
-# todo
-
-
+for file in cpps:
+    preprocess(file)
 
 # build ast trees
 ast_names = []
@@ -27,18 +24,15 @@ ast_names = []
 for file in cpps:
     ast_names.append(build_ast(file))
 
-
-
 # parse ast trees and count allocations
-all_allocations = [0, 0, 0]
+all_allocations = np.matrix([[0, 0], [0, 0], [0, 0]])
 
 for ast_name in ast_names:
+    if not os.path.exists(ast_name):
+        continue
+
     allocations = count_allocations(parse(ast_name))
-    all_allocations[0] += allocations[0]
-    all_allocations[1] += allocations[1]
-    all_allocations[2] += allocations[2]
-
-
+    all_allocations += allocations
 
 # print results
 print(all_allocations)
