1095f0e9035e6726dc038ea5e45e85bb850916fd
Eugene Dar <evgenedar@mail.ru>
2022-12-17 20:40:56 +0300

    preprocessor implemented

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -2,6 +2,13 @@
 # this file should preprocess all cpps:
 # 1. wrap all includes by flags-variables
 
-def preprocess():
-    # do nothing
-    return 0
+def preprocess(file_name):
+    file1 = open(file_name, "r")
+
+    while True:
+        line = file1.readline()
+        if not line:
+            break
+        print(line.strip())
+
+    file1.close()
