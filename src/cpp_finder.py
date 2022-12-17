56104f4f7b3205654d59330f4478d6e9bedebee5
Eugene Dar <evgenedar@mail.ru>
2022-12-17 16:13:02 +0300

    cpp finder implemented

--- src/cpp_finder.py
+++ src/cpp_finder.py
@@ -1,4 +1,14 @@
 # Step 2
 # this function should inspect 'data' folder and return list of paths to cpp files
+import os
 
-# def get_cpps_list ():
+def get_cpps_list ():
+    cpp_files = []
+
+    for root, dirs, files in os.walk("../data"):
+        for file in files:
+            if file.endswith(".cc") or file.endswith(".cpp"):
+                cpp_files.append(str(os.path.join(root, file)))
+                print(os.path.join(root, file))
+
+    return cpp_files
