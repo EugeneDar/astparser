286a7a6d45dc721db3ae132e97f94f3042d57831
Eugene Dar <evgenedar@mail.ru>
2022-12-17 16:41:44 +0300

    ast builder fixed

--- src/ast_builder.py
+++ src/ast_builder.py
@@ -1,8 +1,10 @@
 # Step 4
 # this function should build ast
 import os
+import random
 
 def build_ast(source_name):
-    output_name = '../input' + source_name.lstrip('../data')
+    output_name = '../input/' + os.path.basename(source_name) + '___' + str(random.random())
+    print(output_name)
     command = 'clang++ -Xclang -ast-dump -fsyntax-only ' + source_name + ' > ' + output_name
     os.system(command)
