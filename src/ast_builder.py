9ad294897584907aad6f69ec4af0dc6603b36377
Eugene Dar <evgenedar@mail.ru>
2022-12-17 16:31:31 +0300

    ast builder implemented

--- src/ast_builder.py
+++ src/ast_builder.py
@@ -1,2 +1,8 @@
 # Step 4
 # this function should build ast
+import os
+
+def build_ast(source_name):
+    output_name = '../input' + source_name.lstrip('../data')
+    command = 'clang++ -Xclang -ast-dump -fsyntax-only ' + source_name + ' > ' + output_name
+    os.system(command)
