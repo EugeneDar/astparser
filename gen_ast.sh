da414b2f7b15c8721e6f82b3141e1f220266bfdb
Eugene Dar <evgenedar@mail.ru>
2023-01-21 21:28:40 +0300

    comment changed

--- gen_ast.sh
+++ gen_ast.sh
@@ -1,6 +1,6 @@
 export TERM=xterm-mono
 
-#clang++ -Xclang -ast-dump -fsyntax-only data/simple_heap_allocation.cpp > input/simple_heap_allocation
+#clang++ -Xclang -ast-dump -fsyntax-only examples/simple_heap_allocation.cpp -fno-diagnostics-color > input/simple_heap_allocation
 #clang++ -Xclang -ast-dump -fsyntax-only data/simple.cpp
 
 for file in `find data -type f -name "*"`
