b963a58f25da5e24fc743588d51897ed59ded328
Eugene Dar <evgenedar@mail.ru>
2023-01-21 21:33:26 +0300

    collector fixed for docker

--- src/data_collector.py
+++ src/data_collector.py
@@ -7,13 +7,12 @@ import os
 # maybe we will need repeat 'gh auth login' in command line
 def collect():
     # remove old data
-    command = "rm -rf ../data && mkdir ../data && rm -rf ../input && mkdir ../input"
+    command = "rm -rf /data && mkdir /data && rm -rf /input && mkdir /input"
     os.system(command)
 
-    repos = set()
-
     username = "eugenedar"
-    token = open("../git_token.txt", "r").readline()
+    token = ''
+    # token = open("../git_token.txt", "r").readline()
     url = 'https://api.github.com/search/repositories?q=language:cpp&page:1&per_page=30'
 
     for page in range(1, 2):
@@ -22,19 +21,14 @@ def collect():
 
         for item in r.json()['items']:
             size = item['size']  # size in Kb
-            if size > 60000 or size < 3000:
+            if size > 5000 or size < 2000:
                 continue
 
             clone_url = item['clone_url']
             full_name = item['full_name']
 
-            command = "git clone " + clone_url + " ../data/" + full_name
+            command = "git clone " + clone_url + " /data/" + full_name
             os.system(command)
 
         url = r.links['next']['url']
         print(url)
-
-    # print('Repos count:', len(repos))
-    # for name in repos:
-    #     command = "gh repo clone " + name + " ../data/" + name
-    #     os.system(command)
